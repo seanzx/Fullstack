dbed822f7c45a434c878c6eb8fe75ca7583029da
seanzx <31815757+seanzx@users.noreply.github.com>
2017-10-15 15:37:38 -0700

    Rewise

--- Web Crawler/douban.py
+++ Web Crawler/douban.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
@@ -5,13 +6,61 @@ sys.setdefaultencoding("utf8")
 import urllib
 import urllib2
 import json
+from bs4 import BeautifulSoup
+
 
 tags = []
 url = 'https://movie.douban.com/j/search_tags?type=movie&source=index'
-
+# get request
 request = urllib2.Request(url=url)
 response = urllib2.urlopen(request, timeout=20)
 result = json.loads(response.read())
 tags = result['tags']
-for item in tags:
-	print item
+# movies list store all movies information
+movies = []
+start = 0;
+# process all kind of movies
+for tag in tags:
+	while 1: 
+		# get request to get all movies url
+		url = 'https://movie.douban.com/j/search_subjects?type=movie&tag=' + tag + '&page_limit=20&page_start=' + str(start)
+		request = urllib2.Request(url=url)
+		response = urllib2.urlopen(request, timeout=20)
+		result = response.read()
+		result = json.loads(result)
+		result = result['subjects']
+		start += 20
+		# loop until there is no movies
+		if len(result) == 0:
+			break
+		# add all movies into list
+		for item in result:
+			movies.append(item)
+
+# open a file to write in data
+fw = open('movies.txt', 'w')
+# movies information includes film director, screenwriter and actor
+fw.write('Film' + '\t' + 'director' + '\t' + 'screenwriter' + '\t' + 'actor' + '\n')
+# search all movies' details
+for x in xrange(0,len(movies)):
+	# get request to search details information
+	url = movies[x]['url']
+	request = urllib2.Request(url=url)
+	response = urllib2.urlopen(request, timeout=20)
+	result = response.read()
+	html = BeautifulSoup(result)
+	# try to search director, screenwriter and actor
+	try:
+		value = html.select('#info span.attrs')
+	except Exception, e:
+		# only write movies name if other information is not accessible
+		fw.write(str(movies[x]['title']))
+	else:
+		movies[x]['director'] = value[0].get_text()
+		movies[x]['screenwriter'] = value[1].get_text()
+		movies[x]['actor'] = value[2].get_text()
+		print str(movies[x]['title'] + '\t' +movies[x]['director'])
+		fw.write(str(movies[x]['title'] + '\t' +movies[x]['director'] + '\t' +movies[x]['screenwriter'] + '\t' +movies[x]['actor']+'\n'))
+	finally:
+		pass
+fw.close()
---
+++ Web Crawler/readme.txt
@@ -0,0 +1,11 @@
+This is a project for webCrawler
+Oct 15, 2017
+***************************************************************
+webCravler.py is basic implementation of two kinds of request.
+douban.py is a realization of web crawler in douban, and douban 
+is a website providing varieties of movies information.
+movies.txt is the result affer running douban.py
+
+***************************************************************
+For now, I only stored film name, director, screenwriter and actor information in
+every movies. 
